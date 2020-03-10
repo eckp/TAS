b7ee261d04e8e7066ddf71d94f26e2b6714bc126
Paul <Paul@Astronomics.local>
2020-03-10 21:34:32 +0100

    Removed the old exponential regression, and adjusted the intitial guesses to better reflect the actual parameters. (a can be seen as the difference between initial temperature and substrate temperature, b is the "cooling rate"? close to 0.8 and c is around the substrate temperature)

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ exp_back = [generate_back(i) for i in range(9)]
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 # exponential regression of the cooling rate
-for i, exp in enumerate(exp_back[::2]):
+for i, exp in enumerate(exp_back[::1]):
     x = np.arange(len(exp.tow1))
     y = exp.tow1[:,300]
     #print(list(zip(x,y)))
     plt.plot(x, y, c=colors[i])
-    plt.plot(x, exp_regression(x, y)(x), c=colors[i], linestyle='--')
-    plt.plot(x, exp_lin_regression(x, y, p0=(y[0], 0.9, y[-1]))(x), c=colors[i], linestyle=':')
+    #plt.plot(x, exp_regression(x, y)(x), c=colors[i], linestyle='--')
+    plt.plot(x, exp_lin_regression(x, y, p0=(y[0]-y[-1], 0.8, y[-1]))(x), c=colors[i], linestyle=':')
 plt.show()

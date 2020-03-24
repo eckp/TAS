83e8dd16484dc1feaeb5f26a80ed56485c80deea
Paul <Paul@Astronomics.local>
2020-03-24 10:39:28 +0100

    Improved the plotting in regression.py

--- regression.py
+++ regression.py
@@ -1,7 +1,4 @@
-'''Provides a function that fits an exponential curve of the form
-y(x) = a*b**x
-to the given data points
-'''
+'''Provides functions that fit exponential curves to a given set of data points.'''
 
 import numpy as np
 from scipy import optimize
@@ -9,32 +6,39 @@ from scipy import optimize
 def exp_regression(x, y, p0=None):
     '''Accepts two lists of x and y values of the data points,
     with an optional list of initial guesses for the parameters.
-    Returns the fitted function y(x) = a*b**x'''
+    Returns the fitted function y(x) = a*b**x and the parameters for later use'''
     x = np.array(x)
     y = np.array(y)
     (a, b), covariance = optimize.curve_fit(lambda t,a,b: a*b**x, x, y, p0=p0)
     print(a, b)
-    return lambda x_: a*b**x_
+    func = lambda x_: a*b**x_
+    return func, (a, b)
 
 def exp_lin_regression(x, y, p0=None):
     '''Accepts two lists of x and y values of the data points,
     with an optional list of initial guesses for the parameters.
-    Returns the fitted function y(x) = a*b**x + c'''
+    Returns the fitted function y(x) = a*b**x + c and the parameters for later use'''
     x = np.array(x)
     y = np.array(y)
     (a, b, c), covariance = optimize.curve_fit(lambda t,a,b,c: a*b**x+c, x, y, p0=p0)
     print(a, b, c)
-    return lambda x_: a*b**x_+c
+    func = lambda x_: a*b**x_+c
+    return func, (a, b, c)
 
 
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
-    func = lambda x: 10*0.5**x
+    a, b, c = 34, 0.55, 10
+    func = lambda x: a*b**x+c
     x = np.linspace(0,10,50)
     y = func(x)
-    plt.plot(x, y)
+    plt.plot(x, y, label=f'original: y(x) = {a}*{b}**x+{c}')
     y_noised = y + np.random.normal(size=x.size)
-    plt.plot(x, y_noised)
-    y_regressed = exp_regression(x, y_noised)(x)
-    plt.plot(x, y_regressed)
+    plt.plot(x, y_noised, label='original + noise')
+    regress = exp_lin_regression(x, y_noised)
+    y_regressed = regress[0](x)
+    plt.plot(x, y_regressed, label=f'regressed: y(x) = {round(regress[1][0], 2)}*{round(regress[1][1], 2)}**x+{round(regress[1][2], 2)}')
+    #plt.annotate(f'y(x) = {round(regress[1][0], 2)}*{round(regress[1][1], 2)}**x+{round(regress[1][2], 2)}',
+    #             (x[int(len(x)*0.2)]*1.2, y_regressed[int(len(x)*0.2)]*1.2))
+    plt.legend()
     plt.show()

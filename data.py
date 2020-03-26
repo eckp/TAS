24f0b33397dea326146b7e5ae247626ad79edcab
DaanAlmekinders <57629146+DaanAlmekinders@users.noreply.github.com>
2020-03-26 11:12:45 +0100

    Create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+'''File containing some handy data that will probably be used by multiple scripts'''
+
+# Experiment parameters (laser power, compaction force)
+experiment_params = np.array([[1300, 100],[1300, 500],[1300, 1000],\
+                              [1500, 100],[1500, 500],[1500, 1000],\
+                              [1750, 100],[1750, 500],[1750, 1000]])

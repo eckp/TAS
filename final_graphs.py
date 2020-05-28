8bb23118cdf76f4fb00b42ea9d1ef623c4d58223
Paul <37733348+eckp@users.noreply.github.com>
2020-05-28 16:04:49 +0200

    intermediate commit

--- final_graphs.py
+++ final_graphs.py
@@ -6,6 +6,7 @@ from nip_point_front import *
 from nip_point_back import *
 from cooling_rate import *
 
+# some lists of styles to use for grouping points
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 linestyles = (':', '-.', '--', '-')
 markers = ('o', '^', 's', 'v')
@@ -36,52 +37,7 @@ def plot_summary(values, error_measure, x_label, x_ticks, y_label, legend_names,
         ax.legend()
     plt.show()
 
-
-def plot_all_cr(all_cr, hlines={}):
-    fig, axes = plt.subplots(3,3, figsize=(12,12))#, sharex=True, sharey=True)
-    for exp_idx, (exp_cr, ax) in enumerate(zip(all_cr, axes.flatten())):
-        for tow_idx, tow_cr in enumerate(exp_cr):
-            exp_params = experiment_params[exp_idx]
-            ax.set_title(f'Experiment {exp_idx+1}: power = {exp_params[0]} W, force = {exp_params[1]} N')
-            ax.plot(list(range(*sample_range)), tow_cr, c=colors[tow_idx])
-            for line, style in zip(hlines, linestyles):
-                ax.axhline(hlines[line][exp_idx][tow_idx], c=colors[tow_idx], linestyle=style, label=line)
-    fig.legend(handles=[plt.Line2D([0], [0], c=colors[idx], label=f'tow nr. {idx*2+1}') for idx in range(tow_idx+1)] +
-                       [plt.Line2D([0], [0], c='k', ls=linestyle, label=name) for name, linestyle in zip(hlines, linestyles[0:len(hlines)])])
-    fig.tight_layout()
-    plt.show()
     
-def old_plot_summary(averaged_points, laser_x=False):
-    '''Accept list of 9 experiments with list of cooling rates of 4 tows each.
-    Setting laser_x=True invert the plot in the sense that there will be 
-    three lines of different force, and that the laser power will be on the x-axis.'''
-    fig, axes = plt.subplots(2,2, figsize=(10,10), sharex=False, sharey=True)
-    data = np.array(averaged_points).reshape(3,3,4)
-    experiment_parameters = np.array(experiment_params).reshape(3,3,2)
-    for tow_idx, (tow_data, ax) in enumerate(zip(data.T, axes.flatten()[[0,2,3,1]])):
-        if laser_x:
-            tow_data = tow_data
-            exp_prms = np.transpose(experiment_parameters, (1,0,2))
-            x_ticks_idx = 0
-            x_label = 'Laser power $[W]$'
-            line_label = lambda force: f'{force} $N$'
-        else:
-            tow_data = tow_data.T
-            exp_prms = experiment_parameters
-            x_ticks_idx = 1
-            x_label = 'Compaction force $[N]$'
-            line_label = lambda power: f'{power} $W$'
-        for settings, line in zip(exp_prms, tow_data):
-            x = settings[:,x_ticks_idx]
-            ax.set_xlabel(x_label)
-            ax.set_ylabel('Cooling speed $k\ [s^{-1}]$')
-            ax.set_title(f'Cooling speed for {("outer", "inner")[tow_idx in (1,2)]} tow {tow_idx*2+1}')
-            ax.set_xticks(x)
-            ax.plot(x, line, label=line_label(settings[0,~x_ticks_idx]))
-            ax.fill_between(x, line*0.8, line*1.2, alpha=0.2)  # use plt.errorbar
-            ax.legend()
-    fig.tight_layout()
-    plt.show()
 
 
 if __name__ == "__main__":

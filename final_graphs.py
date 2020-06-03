a095c69f1be122b7b4e30f42e050032948cef5c8
Paul <37733348+eckp@users.noreply.github.com>
2020-06-03 13:31:22 +0200

    Added the axis labeling for shared axes when only one subplot is created. Made the plot_summary function switch between an overhead legend for multiple subplots, and an in-axis legend for a single subplot (as the overhead space was not wide enough in that case) Also condensed the main part down by looping over the different topics with custom data and y-axis labels. x-axis labels, legend entries etc are the same for all topics.

--- final_graphs.py
+++ final_graphs.py
@@ -34,61 +34,33 @@ def plot_summary(values, error_bars, x_label, x_ticks, y_label, legend_names, sh
         for line, errors, name, marker in zip(tow_values, tow_errors, legend_names, markers):
             ax.errorbar(x_ticks, line, errors, ls=(0,(2,7)), lw=1, marker=marker, capsize=3, label=name)
         # set the individual subplot labels if axes are not shared
-        if not sharex:
+        if columns == 1 and not sharex:
             ax.set_xlabel(x_label)
             ax.set_xticks(x_ticks)
-        if not sharey:
+        if rows == 1 and not sharey:
             ax.set_ylabel(y_label)
         ax.set_title(f'Tow nr. {tow_idx*2+1}')
         handles, labels = ax.get_legend_handles_labels()
-        #ax.legend()
+        if rows*columns == 1:  # if there is only one subplot
+            ax.legend([h[0] for h in handles], labels)
     fig.tight_layout()
-    fig.subplots_adjust(top=0.9)
-    # put the legend on the top across all subplots
-    fig.legend([h[0] for h in handles], labels,  # only markers and line style, no error bars in legend
-               bbox_to_anchor=(0.05, 0.95, 0.9, 0.), loc='lower left', ncol= len(labels), mode="expand", borderaxespad=0)  
+    if rows*columns != 1:  # if there is more than one subplot
+        fig.subplots_adjust(top=0.9)
+        # put the legend on the top across all subplots
+        fig.legend([h[0] for h in handles], labels,  # only markers and line style, no error bars in legend
+                   bbox_to_anchor=(0.05, 0.95, 0.9, 0.), loc='lower left', ncol= len(labels), mode="expand", borderaxespad=0)  
     plt.show()
 
     
 
 
 if __name__ == "__main__":
-    avg_size = (5,5)
-    # nip point summary plot
-    nip_values, nip_err, *_ = front_arrays()
-    plot_summary(nip_values, nip_err, 'Compaction force $[N]$', experiment_params[:3,1],
-                 'Average temperature $[^\circ C]$', [f'Laser power = {power} W' for power in experiment_params[::3,0]])
-    # nip average over all tows
-    avg_nip_values = np.array([np.mean(nip_values, axis=0)])
-    avg_nip_err = np.array([np.mean(nip_err, axis=0)])
-    plot_summary(avg_nip_values, avg_nip_err, 'Compaction force $[N]$', experiment_params[:3,1],
-                 'Average temperature $[^\circ C]$', [f'Laser power = {power} W' for power in experiment_params[::3,0]], size=avg_size)
-    raise RuntimeError
-    # exit point summary plot
-    exit_values, exit_err, *_ = back_arrays()
-    plot_summary(exit_values, exit_err, 'Compaction force $[N]$', experiment_params[:3,1],
-                 'Average temperature $[^\circ C]$', [f'Laser power = {power} W' for power in experiment_params[::3,0]])
-    # exit average over all tows
-    avg_exit_values = np.array([np.mean(exit_values, axis=0)])
-    avg_exit_err = np.array([np.mean(exit_err, axis=0)])
-    plot_summary(avg_exit_values, avg_exit_err, 'Compaction force $[N]$', experiment_params[:3,1],
-                 'Average temperature $[^\circ C]$', [f'Laser power = {power} W' for power in experiment_params[::3,0]], size=avg_size)
-    # nip-exit difference summary plot
-    diff_values, diff_err, *_ = diff_arrays()
-    plot_summary(diff_values, diff_err, 'Compaction force $[N]$', experiment_params[:3,1],
-                 'Temperature difference $[^\circ C]$', [f'Laser power = {power} W' for power in experiment_params[::3,0]])
-    # nip-exit average over all tows
-    avg_diff_values = np.array([np.mean(diff_values, axis=0)])
-    avg_diff_err = np.array([np.mean(diff_err, axis=0)])
-    plot_summary(avg_diff_values, avg_diff_err, 'Compaction force $[N]$', experiment_params[:3,1],
-                 'Average temperature $[^\circ C]$', [f'Laser power = {power} W' for power in experiment_params[::3,0]], size=avg_size)
-    # cooling rate summary plot
+    ylabels = (*('temperature $[^\circ C]$',)*2, 'temperature difference $[^\circ C]$', 'cooling constant $k\ [s^{-1}]$')
     cr_values, cr_err, *_ = calc_stats(all_cooling_rates=load_cached_cr()[3])
     cr_values, cr_err = cr_values.T.reshape((4,3,3)), cr_err.T.reshape((4,3,3))
-    plot_summary(cr_values, cr_err, 'Compaction force $[N]$', experiment_params[:3,1],
-                 'Average cooling constant $k\ [s^{-1}]$', [f'Laser power = {power} W' for power in experiment_params[::3,0]])
-    # cooling rate average over all tows
-    avg_cr_values = np.array([np.mean(cr_values, axis=0)])
-    avg_cr_err = np.array([np.mean(cr_err, axis=0)])
-    plot_summary(avg_cr_values, avg_cr_err, 'Compaction force $[N]$', experiment_params[:3,1],
-                 'Average cooling constant $k\ [s^{-1}]$', [f'Laser power = {power} W' for power in experiment_params[::3,0]], size=avg_size)
+    data = (front_arrays(), back_arrays(), diff_arrays(), (cr_values, cr_err))
+    for ylabel, (values, err, *_) in zip(ylabels, data):
+        plot_summary(values, err, 'Compaction force $[N]$', experiment_params[:3,1],
+                     ylabel[0].upper()+ylabel[1:], [f'Laser power = {power} W' for power in experiment_params[::3,0]])
+        plot_summary([np.mean(values, axis=0)], [np.mean(err, axis=0)], 'Compaction force $[N]$', experiment_params[:3,1],
+                     'Average '+ylabel, [f'Laser power = {power} W' for power in experiment_params[::3,0]], size=(5,5))
